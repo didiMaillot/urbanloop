a02a3e8f051d3be5c9414998402e3803e6b1b159
gianlucca <gianlucca.oliveira123@gmail.com>
2025-02-14 12:03:52 +0100

    consigne vitesse

--- UrbanloopConsigne/src/main.cpp
+++ UrbanloopConsigne/src/main.cpp
@@ -1,23 +1,25 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define BAUD_RATE 9600
 #define RX_PIN 16
 #define TX_PIN 17
 
+int vitesse() {
+  return rand() % 255 + 1;
+}
 
 void setup() {
   Serial.begin(9600);
-  
   Serial2.begin(BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN);
+  srand(time(NULL));
   Serial2.write("Hello World!");
 }
 
 void loop() {
-  Serial2.print('a');
-  if(Serial2.available()) {
-    int c = Serial2.read();
-      Serial.print((char)c);
-  }
-
+  Serial2.write(vitesse());
+  delay(5000);
 }
+
